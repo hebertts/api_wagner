d304ac4635ac014c42a254418e05d63d94a4a365
hebertts <mantahebert01@gmail.com>
2025-11-28 19:16:29 -0300

    front

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 from flasgger import Swagger
 from datetime import timedelta
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+
 
 # ---------------- CONFIGURAÇÕES ----------------
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///items.db"
